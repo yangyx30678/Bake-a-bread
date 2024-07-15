cc38d859cc48428e1e8ae1371f02f4ee9041c70e
yangyx30678 <yangyx0000001@gmail.com>
2024-07-15 17:36:01 +0800

    init

---
+++ src/main/java/yangyx30678/bread/BakeABreadDataGenerator.java
@@ -0,0 +1,11 @@
+package yangyx30678.bread;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BakeABreadDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
