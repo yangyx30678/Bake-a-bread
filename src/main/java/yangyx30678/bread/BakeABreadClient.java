ebb70d8ed1e8320e409752dc136b5398045bce1c
yangyx30678 <yangyx0000001@gmail.com>
2024-07-16 02:05:19 +0800

    Very important changes

---
+++ src/main/java/yangyx30678/bread/BakeABreadClient.java
@@ -0,0 +1,10 @@
+package yangyx30678.bread;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BakeABreadClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
