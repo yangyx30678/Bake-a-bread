4721ba1374e8a138dfbed3426a64c86a00a689bf
yangyx30678 <yangyx0000001@gmail.com>
2024-07-16 18:15:08 +0800

    v1.0.1 Increase hunger

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Bake-a-bread
-a fabric minecraft mod
+# Bake a bread 烤麵包
+新增具有原生感的烤麵包
+add a vanilla like baked bread
+
+## Features 特色
+* 支援熔爐 Support smelting
+* 支援營火 Support campfire cooking
+* 支援煙燻爐 Support smoking
+* 支援英文與繁體中文 Support English and Traditional Chinese
+
+## Effect 效果
+* hunger : 7
+* saturation : 0.6
--- src/main/java/yangyx30678/bread/item/ModFoodComponents.java
+++ src/main/java/yangyx30678/bread/item/ModFoodComponents.java
@@ -6,6 +6,6 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
-    public static final FoodComponent BAKED_BREAD = new FoodComponent.Builder().hunger(5).saturationModifier(0.6f).build();
+    public static final FoodComponent BAKED_BREAD = new FoodComponent.Builder().hunger(7).saturationModifier(0.6f).build();
 
 }
