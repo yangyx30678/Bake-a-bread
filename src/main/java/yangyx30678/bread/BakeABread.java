efa3c7b0009199bb325ab8e213b8e94270ffdaa5
yangyx30678 <yangyx0000001@gmail.com>
2024-07-16 16:41:20 +0800

    v1.0

--- src/main/java/yangyx30678/bread/BakeABread.java
+++ src/main/java/yangyx30678/bread/BakeABread.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import yangyx30678.bread.item.ModItemGroups;
 import yangyx30678.bread.item.ModItems;
 
 public class BakeABread implements ModInitializer {
@@ -15,6 +14,5 @@ public class BakeABread implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
 		ModItems.registerModItems();
-//		ModItemGroups.registerItemGroups();
 	}
 }
